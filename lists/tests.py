71506acc9edab37a1d6325325aeef0579090a66d
sleeperbus <sleeperbus@gmail.com>
2020-11-25 08:42:56 +0900

    * view test 에서 입력을 passing through 하는 것에 model 에 저정하는 것으로 변경 * home_page view 는 every request 마다 item 을 생성한다. 고쳐야 한다.

--- lists/tests.py
+++ lists/tests.py
@@ -13,6 +13,11 @@ class HomePageTest(TestCase):
 
     def test_can_save_a_POST_request(self):
         response = self.client.post('/', data={'item_text': 'A new list item'})
+
+        self.assertEqual(Item.objects.count(), 1)
+        new_item = Item.objects.first()
+        self.assertEqual(new_item.text, 'A new list item')
+
         self.assertIn('A new list item', response.content.decode())
         self.assertTemplateUsed(response, 'home.html')
 
@@ -34,3 +39,4 @@ class ItemModelTest(TestCase):
         second_saved_item = save_items[1]
         self.assertEqual(first_saved_item.text, 'The first (ever) list item')
         self.assertEqual(second_saved_item.text, 'Item the second')
+
