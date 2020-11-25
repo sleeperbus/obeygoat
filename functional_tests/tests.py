0c1ef9eff48ab4eddf4ba5196a8702f3cccd6cbb
sleeperbus <sleeperbus@gmail.com>
2020-11-25 09:59:53 +0900

    * functional test 를 분리한다.   * python 의 기본 unittest 에서 django.test.LiveServerTestCase 로 변경한다.     * test db가 자동으로 생성되고 파괴된다.

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -1,10 +1,10 @@
 import time
-import unittest
+from django.test import LiveServerTestCase
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 
 
-class NewVisitorTest(unittest.TestCase):
+class NewVisitorTest(LiveServerTestCase):
     def setUp(self) -> None:
         self.browser = webdriver.Chrome()
 
@@ -19,7 +19,7 @@ class NewVisitorTest(unittest.TestCase):
     def test_can_start_a_list_and_retrieve_it_later(self):
         # Edith has heard about a cool new online to-do app. She goes
         # to check out its homepage
-        self.browser.get('http://localhost:8000')
+        self.browser.get(self.live_server_url)
 
         # She notices the page title and header mention to-do lists
         self.assertIn('To-Do', self.browser.title)
@@ -65,8 +65,5 @@ class NewVisitorTest(unittest.TestCase):
         # Satisfied, she goes back to sleep
 
 
-
-if __name__ == '__main__':
-    unittest.main()
 
 
