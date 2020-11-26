f612e2280d7a9505da37f2676cbcbcf02dd87ef0
sleeperbus <sleeperbus@gmail.com>
2020-11-26 23:23:56 +0900

    * unit test   * 존재하는 list 에 새로운 item 을 추가

--- lists/tests.py
+++ lists/tests.py
@@ -49,6 +49,29 @@ class NewListTest(TestCase):
         self.assertRedirects(response, f'/lists/{new_list.id}/')
 
 
+class NewItemTest(TestCase):
+    def test_can_save_a_POST_request_to_an_existing_list(self):
+        other_list = List.objects.create()
+        correct_list = List.objects.create()
+
+        self.client.post(f'/lists/{correct_list.id}/add_item',
+                         {'item_text': 'A new item for existing list'})
+
+        self.assertEqual(Item.objects.count(), 1)
+        new_item = Item.objects.first()
+        self.assertEqual(new_item.list, correct_list)
+        self.assertEqual(new_item.text, 'A new item for existing list')
+
+    def test_redirects_to_list_view(self):
+        other_list = List.objects.create()
+        correct_list = List.objects.create()
+
+        response = self.client.post(f'/lists/{correct_list.id}/add_item',
+                         {'item_text': 'A new item for existing list'})
+
+        self.assertRedirects(response, f'/lists/{correct_list.id}/')
+
+
 class ListAndItemModelTest(TestCase):
     def test_saving_and_retrieving_items(self):
         list_ = List()
