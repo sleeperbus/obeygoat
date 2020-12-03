f171a62119e68000fe691d2c12fad58e4bcb191f
sleeperbus <sleeperbus@gmail.com>
2020-12-04 06:49:03 +0900

    * item input 을 위한 form 추가

--- lists/forms.py
+++ lists/forms.py
@@ -2,6 +2,8 @@ from django import forms
 
 
 class ItemForm(forms.Form):
-    item_text = forms.CharField(widget=forms.fields.TextInput(attrs={
-        'placeholder': 'Enter a to-do item',
-    }))
+    # item_text = forms.CharField(widget=forms.fields.TextInput(attrs={
+    #     'placeholder': 'Enter a to-do item',
+    # }))
+    item_text = forms.CharField()
+    item_text.widget.attrs.update({'placeholder': 'Enter a to-do item'})
