983ddc73a996685838356d2c3e8be86c1882033e
sleeperbus <sleeperbus@gmail.com>
2020-12-08 07:04:53 +0900

    * javascript test 시작   * qunit test 와 jquery 를 사용해서 테스트를 시작한다.

--- functional_tests/test_list_item_validation.py
+++ functional_tests/test_list_item_validation.py
@@ -51,7 +51,6 @@ class ItemValidationTest(FunctionalTest):
 
         # She sees a helpful error message
         self.wait_for(lambda: self.assertEqual(
-            self.get_error_element().text,
             "You've already got this in your lists"
         ))
 
