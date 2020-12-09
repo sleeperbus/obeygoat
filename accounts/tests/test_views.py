51dae6ca99cec1eb46bd30e32313123721ae70af
sleeperbus <sleeperbus@gmail.com>
2020-12-10 07:22:07 +0900

    * send_email 외부 함수를 monkeypatch 로 mocking

--- accounts/tests/test_views.py
+++ accounts/tests/test_views.py
@@ -1,8 +1,28 @@
 from django.test import TestCase
 
+import accounts
+
 
 class SendLoginEmailViewTest(TestCase):
     def test_redirects_to_home_page(self):
-        response = self.client.post('/accounts/send_login_email', data={'email': 'healblue@gmail.com'})
+        response = self.client.post('/accounts/send_login_email', data={'email': 'ealblue@example.com'})
         self.assertRedirects(response, '/')
 
+    def test_send_mail_to_address_from_post(self):
+        self.send_mail_called = False
+
+        def fake_send_mail(subject, body, from_email, to_list):
+            self.send_mail_called = True
+            self.subject = subject
+            self.body = body
+            self.from_email = from_email
+            self.to_list = to_list
+
+        accounts.views.send_mail = fake_send_mail
+
+        self.client.post('/accounts/send_login_email', data={'email': 'healblue@example.com'})
+
+        self.assertTrue(self.send_mail_called)
+        self.assertEqual(self.subject, 'Your login link for Superlists')
+        self.assertEqual(self.from_email, 'noreply@superlists')
+        self.assertEqual(self.to_list, ['healblue@example.com'])
--- accounts/views.py
+++ accounts/views.py
@@ -1,6 +1,16 @@
+from django.core.mail import send_mail
 from django.shortcuts import render, redirect
 
 
 def send_login_email(request):
+    email = request.POST['email']
+    send_mail(
+        'Your login link for Superlists',
+        'body text tbc',
+        'noreply@superlists',
+        [email]
+    )
     return redirect('/')
+
+
 
