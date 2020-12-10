6d9db11e466c46fcf1ca59f252859623900fc1c6
sleeperbus <sleeperbus@gmail.com>
2020-12-11 08:50:57 +0900

    * settings 에 logger 추가 * email 을 입력받기 위한 html 코드

--- superlists/settings.py
+++ superlists/settings.py
@@ -104,6 +104,23 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+# LOGGING
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'console': {
+            'level': 'DEBUG',
+            'class': 'logging.StreamHandler',
+        },
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['console'],
+        },
+    },
+    'root': {'level': 'INFO'},
+}
 
 # Internationalization
 # https://docs.djangoproject.com/en/1.11/topics/i18n/
